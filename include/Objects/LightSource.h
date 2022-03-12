1c248b5d7270bf6092072e61900382e84b2f294c
Joao-Luz <jlllima5@gmail.com>
2022-03-12 15:59:44 -0300

    Light sources may now be spotlights

--- include/Objects/LightSource.h
+++ include/Objects/LightSource.h
@@ -10,6 +10,10 @@ private:
     v4f m_ambient_light, m_diffuse_light, m_specular_light;
     int m_light_id;
     bool m_active;
+
+    bool m_spotlight;
+    float m_spot_cutoff;
+    v3f m_spot_direction;
 public:
     LightSource(float x, float y, float z, int id);
     LightSource(v3f position, int id);
@@ -20,6 +24,16 @@ public:
 
     bool active() { return m_active; }
     void set_active(bool val) { m_active = val; }
+
+    bool spotlight() { return m_spotlight; }
+    void set_spotlight(bool val) { m_spotlight = val; }
+
+    float spot_cutoff() { return m_spot_cutoff; }
+    void set_spot_cutoff(float val) { m_spot_cutoff = val; }
+
+    v3f spot_direction() { return m_spot_direction; }
+    void set_spot_direction(v3f spot_direction) { m_spot_direction = spot_direction; }
+    void set_spot_direction(float x, float y, float z) { m_spot_direction = {x, y, z}; }
 
     void display();
 };
--- src/Objects/LightSource.cpp
+++ src/Objects/LightSource.cpp
@@ -1,5 +1,6 @@
 #include "Objects/LightSource.h"
 
+#include <iostream>
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
@@ -21,12 +22,17 @@ m_diffuse_light{diffuse},
 m_specular_light{specular},
 m_light_id{id} {
     m_active = true;
+    m_spot_direction = m_direction;
+    m_spot_cutoff = 30;
+    m_spotlight = false;
 }
 
 void LightSource::display() {
     if (!m_active) {
         glDisable(GL_LIGHT0+m_light_id);
         return;
+    } else {
+        glEnable(GL_LIGHT0+m_light_id);
     }
 
     float light_params[4][4] = {
@@ -42,6 +48,19 @@ void LightSource::display() {
     glLightfv(GL_LIGHT0+m_light_id, GL_DIFFUSE,  &light_params[1][0]);
     glLightfv(GL_LIGHT0+m_light_id, GL_SPECULAR, &light_params[2][0]);
     glLightfv(GL_LIGHT0+m_light_id, GL_POSITION, &light_params[3][0]);
+
+    if (m_spotlight) {
+        float spot_direction[3] = { m_spot_direction.x, m_spot_direction.y, m_spot_direction.z };
+        glLightf(GL_LIGHT0+m_light_id, GL_SPOT_CUTOFF, m_spot_cutoff);
+        glLightfv(GL_LIGHT0+m_light_id, GL_SPOT_DIRECTION, spot_direction);
+        glLightf(GL_LIGHT0+m_light_id, GL_SPOT_EXPONENT, 1.0f);
+    } else {
+        float spot_direction[3] = { 0, 0, -1 };
+        glLightf(GL_LIGHT0+m_light_id, GL_SPOT_CUTOFF, 180.0f);
+        glLightfv(GL_LIGHT0+m_light_id, GL_SPOT_DIRECTION, spot_direction);
+        glLightf(GL_LIGHT0+m_light_id, GL_SPOT_EXPONENT, 0.0f);
+    }
+
 }
 
 }
